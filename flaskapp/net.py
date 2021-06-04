4eb194ddc4246318aeccfd069d900d97ccb8e68b
chabirovri <70947709+chabirovri@users.noreply.github.com>
2021-06-04 21:24:57 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -24,6 +24,7 @@ nh=224
 nw=224
 ncol=3
 # загружаем и создаем стандартную уже обученную сеть keras
+from keras.applications.resnet50 import ResNet50
 visible2 = Input(shape=(nh,nw,ncol),name ='imginp')
 resnet = keras.applications.resnet_v2.ResNet50(include_top=True,  weights='imagenet', input_tensor=visible2,  input_shape=None, pooling=None, classes=1000)
 # чтение изображений из каталога
