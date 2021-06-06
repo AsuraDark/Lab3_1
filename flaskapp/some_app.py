c3ca51aa0af72bc88663e7c1022d998f62c87366
chabirovri <70947709+chabirovri@users.noreply.github.com>
2021-06-06 19:31:50 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -100,7 +100,7 @@ def net():
         _ = plt.legend(['Total', 'Red_Channel', 'Green_Channel', 'Blue_Channel'])
         plt.savefig('./static/my_plot.png')
         
-        b=request.forms.get('openid1')
+        b = form.openid1.data
         im3.enhance(b).save('./static/12.png')
         
         image = plt.imread('./static/12.png')
